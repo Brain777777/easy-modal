709c92c5d788aacecdec9c927965f00c835c1a05
Brain777 <827421256@qq.com>
2022-05-26 21:57:56 +0800

    chore: change playground

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,19 +1,16 @@
-import { Suspense } from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
-import Footer from './components/Footer'
-import routes from '~react-pages'
+import { create } from '../../dist'
 
-export default function App() {
+function App() {
+  create(() => {
+    return <>1</>
+  })
+
   return (
-    <main className="font-sans px-4 py-10 text-center text-gray-700 dark:text-gray-200">
-      <Router>
-        <Routes />
-      </Router>
-      <Footer />
-    </main>
+    <div className="App">
+      <header className="App-header">
+      </header>
+    </div>
   )
 }
 
-function Routes() {
-  return <Suspense fallback={<p>Loading...</p>}>{useRoutes(routes)}</Suspense>
-}
+export default App
--- playground/src/main.tsx
+++ playground/src/main.tsx
@@ -1,14 +1,8 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import App from './App'
 
-import '@unocss/reset/tailwind.css'
-import './styles/main.css'
-import 'uno.css'
-
-const container = document.getElementById('root')
-const root = createRoot(container!)
-root.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
